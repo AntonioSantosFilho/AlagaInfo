0d4f8fa09a1f739225d9a3847d1ea51a18a242bb
AntonioSantosFilho <antonio.santosfilho@discente.univasf.edu.br>
2024-10-04 16:25:07 -0300

    Create-Pages

--- alagainfo/lib/main.dart
+++ alagainfo/lib/main.dart
@@ -1,35 +1,59 @@
 import 'package:flutter/material.dart';
+import 'package:alagainfo/SecondPage.dart';
 
-void main() => runApp(AlagaInfo());
+void main(){
+  runApp(AlagaInfo());  
+}
 
 class AlagaInfo extends StatelessWidget{
+  const AlagaInfo({Key? key}) : super(key: key);
 @override
   Widget build(BuildContext context) {
-    // TODO: implement build
     return MaterialApp(
       home: MyHomePage()
     );
   }
 }
 
+// Remove this custom StatelessWidget class
+
 class MyHomePage extends StatelessWidget{
   @override
   Widget build(BuildContext context){
   return Scaffold( 
     backgroundColor: Colors.white,
-    appBar: AppBar(
-        title: Text('Despesas Pessoais'),
-      ),
       body: Center(
         child: Column(
           mainAxisAlignment: MainAxisAlignment.center,
           children: [
             SizedBox(height: 20), // Espaçamento entre o texto e a imagem
             Image(image: AssetImage('images/Inicio.png')),
-            Text("Versão Inicial"),
+            Container(
+                child: Text("AlagaInfo",
+                textAlign: TextAlign.center,
+                style: TextStyle(
+                  fontSize: 15,
+                  color: Colors.black,),
+                ),
+            ),
+            Container(
+                child: Text("Informe-se & Cuide-se",
+                textAlign: TextAlign.center,
+                style: TextStyle(
+                  fontSize: 15,
+                  color: Colors.black,),
+                ),
+            ),
+            ElevatedButton(
+              onPressed: (){
+                Navigator.push(context,
+                MaterialPageRoute(builder: (context) => SecondPage()));
+              },
+              child: Image(image: AssetImage('images/seta.png'), height: 50, width: 50,))
           ],
         ),
       ),
     );
   }
 }
+
